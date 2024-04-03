b9dc7ef2d192b32fe24e1f321577cc00a6919a37
Aditya Urkude <67952339+Nomad-adi@users.noreply.github.com>
2024-04-03 12:22:33 +0530

    main updated

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
+#home route
 @app.get("/")
 def index():
     return {"Data":{"name":"Aditya Urkude","age":23}}
+
+
+#passing id to func
+@app.get("/about/{id}")
+def about(id):
+    return {"id":id}
+
+
+#passing id as int to func
+@app.get("/blog/{id}")
+def about(id:int):
+    return {"id":id}
+
+# query parms
+@app.get("/stories")
+def stories(limit=10, valid:bool = False, sort:Optional[str]= None):
+    if valid:
+        return {"data":f"{limit} valid stoires in blogs"}
+    else:
+        return {"data":f"{limit} invalid stoires in blogs"}
+
