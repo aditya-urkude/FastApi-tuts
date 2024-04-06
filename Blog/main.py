ec977777f6ff61be9fe62d49903115889de72395
Aditya Urkude <67952339+Nomad-adi@users.noreply.github.com>
2024-04-06 23:30:56 +0530

    user added

--- Blog/main.py
+++ Blog/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schema, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
-
-
+from passlib.context import CryptContext
+from typing import List
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -18,7 +18,7 @@ def get_db():
 
 
 # insert data in db
-@app.post("/blog", status_code=status.HTTP_201_CREATED)
+@app.post("/blog", status_code=status.HTTP_201_CREATED, tags=["Blogs"])
 def create(request: schema.Blog, db:Session = Depends(get_db)):
     new_blog = models.Blogs(title= request.title, body= request.body)
     db.add(new_blog)
@@ -27,13 +27,14 @@ def create(request: schema.Blog, db:Session = Depends(get_db)):
     return new_blog
 
 # fetch data
-@app.get("/blog")
+@app.get("/blog",response_model=schema.Show_blog, tags=["Blogs"])
 def get_blogs(db:Session = Depends(get_db)):
     blog = db.query(models.Blogs).all()
     return blog
 
 # fetch single data
-@app.get("/blog/{id}")
+# response model to fetch specific data
+@app.get("/blog/{id}",response_model=schema.Show_blog, tags=["Blogs"])
 def get_blogs(id, response:Response, db:Session = Depends(get_db)):
     blog = db.query(models.Blogs).filter(models.Blogs.id == id).first()
     if not blog:
@@ -44,14 +45,14 @@ def get_blogs(id, response:Response, db:Session = Depends(get_db)):
     return blog
 
 #remove the data
-@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["Blogs"])
 def delete_blog(id, db:Session = Depends(get_db)):
     db.query(models.Blogs).filter(models.Blogs.id == id).delete(synchronize_session=False)
     db.commit()
     return "done"
 
 #update data
-@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED,response_model=schema.Blog)
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED,response_model=schema.Blog, tags=["Blogs"])
 def update_blog(id:int, request: schema.Blog,db:Session = Depends(get_db)):
     blog = db.query(models.Blogs).filter(models.Blogs.id == id)
     if not blog.first():
@@ -63,3 +64,18 @@ def update_blog(id:int, request: schema.Blog,db:Session = Depends(get_db)):
     print(blog.first())
     return blog.first()
 
+#create user
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+@app.post("/create_user", tags=["Users"])
+def create_user(request: schema.User, db:Session = Depends(get_db)):
+    user = models.Users(name= request.name, email=request.email, password= pwd_context.hash(request.password))
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+#fetch user
+@app.get("/fetch_user", response_model=List[schema.Show_user], tags=["Users"])
+def fetch_user(db:Session = Depends(get_db)):
+    user = db.query(models.Users).all()
+    return user
--- Blog/schema.py
+++ Blog/schema.py
@@ -1,5 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 class Blog(BaseModel):
     title: str
     body: str
+
+class Show_blog(BaseModel):
+    title: str
+    class Config:
+        orm_mode = True
+
+class User(BaseModel):
+    name : str
+    email : str
+    password: str
+
+class Show_user(BaseModel):
+    # specific data to show
+    name: str
+    email: str
+    class Config:
+        orm_mode = True
